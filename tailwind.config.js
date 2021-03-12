8597a84a7aafc92e196a83819bc2b76f4b39b859
Eleazar Torres <eleazar.cacdao.torres@gmail.com>
2021-03-12 05:16:16 -0800

    Configure Tailwind and other js libraries and import assets

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        'japan-A-400': '#22285A',
+        
+      },
+      
+       keyframes: {
+        bounce_idle: {
+          '0%, 100%': { 
+            transform: 'translateY(-5%)',
+            animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)'
+           },
+          '50%': { 
+            transform: 'translateY(0)',
+            animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)'
+           },
+        }
+       }
+
+    },
+  },
+  variants: {
+    extend: {
+      animation: ['hover', 'focus'],
+    },
+  },
+  plugins: [
+    require('tailwindcss-debug-screens'),
+  ],
+}
