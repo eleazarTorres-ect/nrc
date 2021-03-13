31e2e948969c2fff1a911839ed7d7831c6c017a2
Eleazar Torres <eleazar.cacdao.torres@gmail.com>
2021-03-13 02:11:13 -0800

    Mobile responsive for subscription section

--- src/components/subscriptionSection/SubscriptionContainer.jsx
+++ src/components/subscriptionSection/SubscriptionContainer.jsx
@@ -7,12 +7,12 @@ const SubscriptionContainer = () => {
   return (
     <div
       className="h-auto  w-full flex flex-col sm:flex-row justify-center
-                pt-0 pb-20 bg-purple-400 
-                px-2 sm:px-24 md:px-24 lg:px-36"
+                pt-0 pb-20 md:pb-12 xl:pb-20 bg-purple-400 
+                px-4 sm:px-24 md:px-24 lg:px-36"
       id="subscription"
     >
       <div className="flex">
-        <div className="shadow-2xl border bg-white flex shake-slow flex-wrap justify-between px-10 -mt-20 rounded-lg w-full">
+        <div className="shadow-2xl border bg-white flex shake-slow flex-wrap justify-center px-5 md:px-10 mt-10 md:-mt-20 rounded-lg w-full">
           <LeftContainer />
           <RightContainer />
         </div>
